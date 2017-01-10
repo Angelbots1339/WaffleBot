b9b5beded68187ec66a43a95cc3afb3b0bdefb63
Nate <nathanhaashoward@gmail.com>
2017-01-09 17:55:33 -0700

    Added wheel drive

--- WaffleBot/src/org/usfirst/frc/team1339/subsystems/Chassis.java
+++ WaffleBot/src/org/usfirst/frc/team1339/subsystems/Chassis.java
@@ -10,8 +10,11 @@ import edu.wpi.first.wpilibj.Solenoid;
 
 public class Chassis extends SubsystemBase{
 	
-	CANTalon leftOne, leftTwo, rightOne, rightTwo;
-	Solenoid shifter;
+	private CANTalon leftOne, leftTwo, rightOne, rightTwo;
+	private Solenoid shifter;
+	
+	private double mQuickStopAccumulator;
+	private double kTurnSensitivity = 1.0;
 	
 	public Chassis(){
 		leftOne = new CANTalon(Constants.chassisLeftOne);
@@ -24,6 +27,14 @@ public class Chassis extends SubsystemBase{
 	
 	public void initDefaultCommand(){
 		setDefaultCommand(new ArcadeDrive());
+	}
+	
+	public void openShifter(){
+		
+	}
+	
+	public void closeShifter(){
+		
 	}
 	
 	public void arcadeDrive(double throttle, double turn){
@@ -52,5 +63,53 @@ public class Chassis extends SubsystemBase{
 		leftTwo.set(left);
 		rightOne.set(right);
 		rightTwo.set(right);
+	}
+
+	public void wheelDrive(double throttle, double turn, boolean isQuickTurn) {
+		double overPower;
+
+        double angularPower;
+
+        if (isQuickTurn) {
+            if (Math.abs(throttle) < 0.2) {
+                double alpha = 0.1;
+                double limitedWheel = 0;
+                if(turn > 0)
+                	limitedWheel = Math.min(turn, 1.0);
+                else
+                	limitedWheel = Math.max(turn, -1.0);
+                mQuickStopAccumulator = (1 - alpha) * mQuickStopAccumulator + alpha * limitedWheel * 2;
+            }
+            overPower = 1.0;
+            angularPower = turn;
+        } else {
+            overPower = 0.0;
+            angularPower = Math.abs(throttle) * turn * kTurnSensitivity - mQuickStopAccumulator;
+            if (mQuickStopAccumulator > 1) {
+                mQuickStopAccumulator -= 1;
+            } else if (mQuickStopAccumulator < -1) {
+                mQuickStopAccumulator += 1;
+            } else {
+                mQuickStopAccumulator = 0.0;
+            }
+        }
+
+        double right = throttle - angularPower;
+        double left = throttle + angularPower;
+        if (left > 1.0) {
+            right -= overPower * (left - 1.0);
+            left = 1.0;
+        } else if (right > 1.0) {
+            left -= overPower * (right - 1.0);
+            right = 1.0;
+        } else if (left < -1.0) {
+            right += overPower * (-1.0 - left);
+            left = -1.0;
+        } else if (right < -1.0) {
+            left += overPower * (-1.0 - right);
+            right = -1.0;
+        }
+        
+        setMotorValues(left, right);
 	}
 }
